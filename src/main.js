11fc2302f4b4ed8e3e1e750dbb65044f79a532a9
5ar1m <2022umt1495@gmail.com>
2025-08-23 19:54:59 +0530

    complete project

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const logger = require('./utils/logger');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    logger.info(`server started at port ${port}`)
-});
